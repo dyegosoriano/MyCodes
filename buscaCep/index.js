5e6d71a10bda57f0ef28c114514dffe9bf4f651e
Dyego Soriano <dyegodf2@gmail.com>
2020-11-21 15:26:09 -0300

    refatorando buscarCep

--- buscaCep/index.js
+++ buscaCep/index.js
@@ -1,28 +1,36 @@
 const inputCep = document.getElementById('inputCep')
-const outputCep = document.getElementById('outputCep')
 
 inputCep.addEventListener('keyup', handleKeyUp)
 
 async function handleKeyUp () {
+  const outputCep = document.getElementById('outputCep')
   const cep = inputCep.value
 
-  outputCep.innerHTML = ''
-
-  if (cep.length < 8) { return }
+  if (cep.length < 8) return
 
   if (cep.length > 8) {
-    outputCep.innerHTML = 'Você inceriu um CEP inválido'
+    outputCep.innerHTML = 'Você inseriu um CEP inválido'
     return
   }
 
   const dadosCep = await buscarCep(cep)
 
-  outputCep.innerHTML = ceratetHtml(dadosCep)
-
-  console.log(dadosCep)
+  outputCep.innerHTML = createHtml(dadosCep)
 }
 
-function ceratetHtml (cepObj) {
+async function buscarCep (cep) {
+  try {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(response => response.json())
+      .then(response => response)
+
+    return response
+  } catch (error) {
+    console.log(`error.message >>> ${error.message} <<<`)
+  }
+}
+
+function createHtml (cepObj) {
   const { logradouro, bairro, localidade, uf, cep } = cepObj
 
   const htmlRua = `<p><strong>Rua: </strong>${logradouro}</p>`
@@ -31,13 +39,9 @@ function ceratetHtml (cepObj) {
   const htmlUf = `<p><strong>UF: </strong>${uf}</p>`
   const htmlCep = `<p><strong>CEP: </strong>${cep}</p>`
 
-  const html = `<div class='active'>${htmlRua + htmlBairro + htmlCidade + htmlUf + htmlCep}</div>`
+  const html = `<div class='active'>${
+    htmlRua + htmlBairro + htmlCidade + htmlUf + htmlCep
+  }</div>`
 
   return html
-}
-
-function buscarCep (cep) {
-  return fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    .then(response => response.json())
-    .then(response => response)
 }
