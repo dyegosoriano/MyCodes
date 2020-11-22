81d33e49be06c2ffb0cb295a54b424093bff4691
Dyego Soriano <dyegodf2@gmail.com>
2020-11-22 18:14:18 -0300

    refatorando typeWriter

--- typeWriter/typeWriter.js
+++ typeWriter/typeWriter.js
@@ -1,23 +1,26 @@
-// Script que adiciona edeito da "Maquina de Escrever"
+// Script que adiciona efeito da "Maquina de Escrever"
 
 const titulo = document.querySelector('h1')
 
 function typeWriter (element) {
-  const textoArray = element.innerHTML.split('')
+  const textArray = element.innerHTML.split('')
 
   element.innerHTML = ''
 
-  textoArray.forEach((letra, i) => {
-    setTimeout(() => element.innerHTML += letra, 75 * i)
+  textArray.forEach((letter, index) => {
+    setTimeout(() => (element.innerHTML += letter), 75 * index)
   })
 }
 
 // Caso estiver tendo problemas com performance, utilize o FOR loop como abaixo no local do forEach
-// function typeWriter(element) {
-//   const textoArray = element.innerHTML.split('');
-//   element.innerHTML = '';
-//   for(let i = 0; i < textoArray.length; i++) {
-//     setTimeout(() => element.innerHTML += textoArray[i], 75 * i);
+// function typeWriter (element) {
+//   const textArray = element.innerHTML.split('')
+//   const textLength = textArray.length
+
+//   element.innerHTML = ''
+
+//   for (let i = 0; i < textLength; i++) {
+//     setTimeout(() => element.innerHTML += textArray[i], 75 * i)
 //   }
 // }
 
