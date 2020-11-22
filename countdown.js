b88a2152f701f36a32914b8e1a381b3fa7461a25
Dyego Soriano <dyegodf2@gmail.com>
2020-11-22 16:49:07 -0300

    countDown

---
+++ countdown.js
@@ -0,0 +1,53 @@
+// Como usar
+// Primeiro você deve importar a Classe Countdown
+// import Countdown from './countdown.js';
+// Depois você deve declarar o Objeto Countdown em seu projeto
+// const diasParaONatal = new Countdown('24 December 2020 23:59:50 GMT-0300');
+
+export default class Countdown {
+  constructor (futureDate) {
+    this.futureDate = futureDate
+  }
+
+  get _actualDate () {
+    return new Date()
+  }
+
+  get _futureDate () {
+    return new Date(this.futureDate)
+  }
+
+  get _timeStampDiff () {
+    return this._futureDate.getTime() - this._actualDate.getTime()
+  }
+
+  get days () {
+    return Math.floor(this._timeStampDiff / (24 * 60 * 60 * 1000))
+  }
+
+  get hours () {
+    return Math.floor(this._timeStampDiff / (60 * 60 * 1000))
+  }
+
+  get minutes () {
+    return Math.floor(this._timeStampDiff / (60 * 1000))
+  }
+
+  get seconds () {
+    return Math.floor(this._timeStampDiff / 1000)
+  }
+
+  get total () {
+    const days = this.days
+    const hours = this.hours % 24
+    const minutes = this.minutes % 60
+    const seconds = this.seconds % 60
+
+    return {
+      days,
+      hours,
+      minutes,
+      seconds
+    }
+  }
+}
